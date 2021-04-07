403cc68a7e1efc6929f31759b8c3f6a0a16283e9
Kenny Li <kenny.wj.li@gmail.com>
2021-04-06 23:05:33 -0400

    fixing up play/pause button

--- src/index.js
+++ src/index.js
@@ -111,7 +111,7 @@ const sampler = new Tone.Sampler({
     // "d4": 'd4.mp3'
 		// "D#4": "Ds4.mp3",
 		// "F#4": "Fs4.mp3",
-		// "A4": "A4.mp3",
+		"A4": "A4.mp3",
 	},
 	release: 1,
 	baseUrl: "https://tonejs.github.io/audio/salamander/",
@@ -134,16 +134,6 @@ const sampler = new Tone.Sampler({
  
     musicArray.push(AMinorScaleWithOctave[Math.floor(delta + dist)])
     
-    
-    // musicArray.forEach(note=>{
-    //   new Tone.Loop(time => {
-    //       sampler.triggerAttackRelease( note,now+1);
-    //   }).start(`${(musicArray.length +1)}n`);
-
-    // })
-
-
-    
     sampler.triggerAttackRelease( AMinorScaleWithOctave[Math.floor(delta + dist)], now + 1);
     ctx.strokeStyle = grad;
     
@@ -157,35 +147,30 @@ const sampler = new Tone.Sampler({
   
   document.getElementById('play-button').onclick = function(){handlePlay()}
 
+  let idx = 1;
+  Tone.Transport.scheduleRepeat(time => {
+    if (idx > musicArray.length){
+      // sampler.triggerRelease(time)
+      // Tone.Transport.stop()
+      idx = 0;
+    }else{
+      sampler.triggerAttack(musicArray[idx-1], time);
+      console.log(musicArray)
+    }
+    idx ++;
+  }, "4n")
+
   function handlePlay(){
-    let index = 1;
-    Tone.Transport.scheduleRepeat(time => {
-      if (index > musicArray.length){
-        // sampler.triggerRelease(time)
-        // Tone.Transport.stop()
-        index = 0;
-      }else{
-        sampler.triggerAttack(musicArray[index-1], time);
-        console.log(musicArray)
-      }
-      index ++;
-    }, "4n")
-    Tone.start()
+    // Tone.start()
     Tone.Transport.start()
-    const now = Tone.now()
-    // function repeat(){
-    //   let step = musicArray.length
-
-    //     setTimeout(function(){
-
-    //       sampler.triggerAttackRelease( musicArray[step - 1], now);
-    //     },100)
-        
-     
-    // }
   }
   
-
+  document.getElementById('pause-button').onclick = function(){handlePause()}
+  
+  function handlePause(){
+    // const now = Tone.now()
+    Tone.Transport.stop()
+  }
 
   function draw(e){
     if (!drawing) return;
@@ -212,107 +197,3 @@ const sampler = new Tone.Sampler({
 
 
 
-// import "./styles/index.scss";
-// import "./images/yoda-stitch.jpg";
-// import canvasExample from "./scripts/canvas";
-// import Square from "./scripts/square";
-// import { DOMExample } from "./scripts/DOMExample";
-// const currentStateObj = {
-//   currentExample: null,
-//   currentEventListeners: [],
-// };
-
-// document.querySelector("#canvas-demo").addEventListener("click", startCanvas);
-// document.querySelector("#DOM-demo").addEventListener("click", startDOM);
-
-// function startDOM() {
-//   unregisterEventListeners();
-//   clearDemo();
-//   currentStateObj.currentExample = "DOMDEMO";
-//   DOMExample();
-// }
-
-// function startCanvas() {
-//   clearDemo();
-//   unregisterEventListeners();
-//   currentStateObj.currentExample = "CANVASDEMO";
-//   const canvas = new canvasExample();
-//   canvas.createCanvas();
-//   const squares = [new Square(canvas.ctx, canvas.coords, canvas.fillColor)];
-
-//   let animating = true;
-
-//   const animation = () => {
-//     canvas.clearCanvas();
-//     if (animating) squares.forEach((sq) => sq.updateSquare(canvas.fillColor));
-//     squares.forEach((sq) => sq.drawSquare());
-//     window.requestAnimationFrame(animation);
-//     squares.forEach((sq) => {
-//       if (sq.coords[0] + sq.coords[2] > window.innerWidth)
-//         sq.reverseAnimation();
-//       if (sq.coords[0] < 0) sq.reverseAnimation();
-//     });
-//   };
-
-//   window.requestAnimationFrame(animation);
-
-//   window.addEventListener("keydown", handleKeyDown);
-//   currentStateObj.currentEventListeners.push([
-//     "window",
-//     "keydown",
-//     handleKeyDown,
-//   ]);
-
-//   window.addEventListener("mousedown", handleMouseDown);
-//   currentStateObj.currentEventListeners.push([
-//     "window",
-//     "mousedown",
-//     handleMouseDown,
-//   ]);
-
-//   function handleKeyDown(event) {
-//     if (event.which === 32) {
-//       event.preventDefault();
-//       squares.forEach((sq) => sq.reverseAnimation());
-//       canvas.setColor(`#${Math.floor(Math.random() * 16777215).toString(16)}`);
-//     }
-//   }
-
-//   function handleMouseDown(event) {
-//     event.preventDefault();
-//     squares.push(
-//       new Square(
-//         canvas.ctx,
-//         canvas.coords.map((co) => co + 25),
-//         canvas.fillColor
-//       )
-//     );
-//     // animating = !animating;
-//   }
-// }
-
-// function unregisterEventListeners() {
-//   while (currentStateObj.currentEventListeners.length) {
-//     let [
-//       selector,
-//       event,
-//       handler,
-//     ] = currentStateObj.currentEventListeners.pop();
-//     if (selector === "window") {
-//       window.removeEventListener(event, handler);
-//       console.log(handler);
-//     } else {
-//       document.querySelector(selector).removeEventListener(event, handler);
-//     }
-//   }
-// }
-
-// function clearDemo() {
-//   if (currentStateObj.currentExample === "CANVASDEMO")
-//     document.body.removeChild(document.querySelector("canvas"));
-//   if (currentStateObj.currentExample === "DOMDEMO") {
-//     [...document.querySelectorAll(".card")].forEach((elem) =>
-//       document.body.removeChild(elem)
-//     );
-//   }
-// }
